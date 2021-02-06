5b1da2de341625c2f65e4793958e1207bd70cc3c
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-02-06 17:09:25 +0200

    the singlton pattern

---
+++ Logger.js
@@ -0,0 +1,30 @@
+class Logger {
+
+    constructor() {
+        this.logs = [];
+    }
+
+    get count() {
+        return this.logs.length;
+    }
+
+    log(message) {
+        const timestamp = new Date().toISOString();
+        this.logs.push({ message, timestamp });
+        console.log(`${timestamp} - ${message}`);
+    }
+
+}
+
+class Singlton{
+    constructor(){
+        if(!Singlton.instace){
+            Singlton.instace=new Logger()
+        }
+    }
+    getInstance(){
+        return Singlton.instace
+    }
+}
+
+module.exports = Singlton;
