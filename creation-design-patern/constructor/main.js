e2e9b0669a1dd8df5bd50d16ebbd9995cacc45fb
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-02-28 21:14:54 +0200

    add main

---
+++ creation-design-patern/constructor/main.js
@@ -0,0 +1,10 @@
+var Task=require('./task')
+
+var task1=new Task('create demo for constructor')
+var task2=new Task('create demo for modules')
+var task3=new Task('create demo for singltons')
+var task4=new Task('create demo for prototypes')
+
+
+task1.complete()
+task2 .save()
